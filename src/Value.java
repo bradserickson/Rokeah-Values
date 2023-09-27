3a56980a40174bc079c9a45716552a705ffe613a
bradserickson <118328953+bradserickson@users.noreply.github.com>
2023-09-26 20:35:16 -0600

    add Value class

--- src/Value.java
+++ src/Value.java
@@ -3,10 +3,12 @@ public class Value {
     //properties
     private String name;
     private int points = 0;
+    private String category;
 
     //constructors
-    public Value(String name){
+    public Value(String name, String category){
         this.name = name;
+        this.category = category;
     }
 
     //getters
